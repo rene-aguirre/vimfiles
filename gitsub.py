de30800f8b436216b3ac3f6be1aa80392e704396
Rene Aguirre <rene.f.aguirre@gmail.com>
2016-02-24 23:07:05 -0800

    [ctrlp] git helper py3 support
    Using universal_newlines in subprocess arguments

--- gitsub.py
+++ gitsub.py
@@ -29,20 +29,20 @@ def list_git_files(prefix, start_dir):
     full_prefix = os.path.join(prefix, start_dir)
     # first print local files
     if not DEBUG_DIRS:
-        for item in subp.check_output(cmd_ls_files).split('\n'):
+        for item in subp.check_output(cmd_ls_files, universal_newlines=True).split('\n'):
             if item:
                 print(os.path.normpath(os.path.join(full_prefix, item)))
     else:
         print(full_prefix)
- 
+
     if os.path.exists('.gitmodules'):
         if DIRECT_ACCESS:
-            for key_value in subp.check_output(cmd_git_subs).split('\n'):
+            for key_value in subp.check_output(cmd_git_subs, universal_newlines=True).split('\n'):
                 dir_item = " ".join( key_value.split(' ')[1:] )
                 if dir_item:
                     list_git_files(full_prefix, dir_item)
         else:
-            for dir_item in subp.check_output(cmd_ls_subs).split('\n'):
+            for dir_item in subp.check_output(cmd_ls_subs, universal_newlines=True).split('\n'):
                 if dir_item:
                     list_git_files(full_prefix, dir_item)
     # restore dir
