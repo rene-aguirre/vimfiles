dfe16bf7f0a27123eb6fb363d8d7c1f796aae215
Rene Aguirre <rene_aguirre@apple.com>
2019-03-27 11:16:11 -0700

    Better install_macos.sh
     - More generic code.
     - Brew cleanup fixes
     - Forced vim-plugged installed when it needs to build cpsm

--- install_macos.sh
+++ install_macos.sh
@@ -1,35 +1,45 @@
+#!/usr/bin/env bash
+
+echo "Symlink config path: ~/.config/nvim/init.vim..."
 ls ~/.config/nvim/init.vim &>/dev/null \
     || { mkdir -p ~/.config/nvim; ln -s `pwd`/vimrc ~/.config/nvim/init.vim; } \
     || { echo "Can't link symlink neovim config"; exit 1; }
-echo "Required packages, check & install..."
+
 which brew \
     || { echo "Please install homebrew first"; exit 1; }
-which nvim \
-    || brew install neovim --force-bottle || exit 1
-which python3 \
-    || brew install python3 --force-bottle || exit 1
-which python2 \
-    || brew install python2 --force-bottle || exit 1
-which cmake \
-    || brew install cmake --force-bottle || exit 1
-which rg \
-    || brew install ripgrep --force-bottle || exit 1
-which git \
-    || brew install git --force-bottle || exit 1
 
-echo "Boost libraries..."
-brew list boost &>/dev/null || brew install boost || exit 1
+echo "Brew update & cleanup."
+brew update && brew cleanup \
+    || { echo "Need root to fix homebrew"; sudo chown -R $(whoami) $(brew --prefix)/* ; } \
+    || brew update && brew cleanup \
+    || exit 1
 
-echo "Nerd Font..."
-ls ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf &>/dev/null || curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -o ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf || exit 1
+BREW_PKGS=( neovim git python3 python2 cmake ripgrep boost shellcheck yamllint )
 
-echo "Python2 pynvim package"
+echo "Homebrew packages, check & install..."
+for PKG_NAME in "${BREW_PKGS[@]}"; do
+    echo "Checking for ${PKG_NAME}."
+    brew list "${PKG_NAME}" &>/dev/null || which "${PKG_NAME}" &>/dev/null || brew install "${PKG_NAME}" --force-bottle || exit 1
+done
+echo "Checking for universal-ctags"
+brew list universal-ctags &>/dev/null || brew install --HEAD universal-ctags/universal-ctags/universal-ctags || exit 1
+
+echo "Checking for Nerd Font."
+ls ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf &>/dev/null \
+    || curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf \
+    -o ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf \
+    || exit 1
+
+echo "Python helper packages"
 pip2 show pynvim &>/dev/null || pip2 install pynvim || exit 1
-
-echo "Python3 pynvim package"
 pip3 show pynvim &>/dev/null || pip3 install pynvim || exit 1
+pip3 show pylint &>/dev/null || pip3 install pylint || exit 1
 
 echo "NeoVim PlugInstall..."
-nvim --headless +PlugInstall! +qa
-ls /usr/local/opt/universal-ctags/bin/ctags &>/dev/null || { echo "Optional: brew install --HEAD universal-ctags/universal-ctags/universal-ctags"; }
+if [ -f ~/.vim/plugged/cpsm/bin/cpsm_cli ]; then
+    nvim --headless +PlugInstall +qa
+else
+    nvim --headless +PlugInstall! +qa
+fi
+
 echo "Done."
