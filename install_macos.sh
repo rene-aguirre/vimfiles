9503cff641f4df63fdf3d2787159e267b51d6b1f
Rene Aguirre <rene_aguirre@apple.com>
2020-01-12 00:03:08 -0800

    mac installer helper fixex
    * Use command instead of which
    * fix python installs and linking

--- install_macos.sh
+++ install_macos.sh
@@ -5,15 +5,17 @@ USRLOCALBIN="/usr/local/bin"
 
 echo "Symlink config path: ~/.config/nvim/init.vim..."
 ls ~/.config/nvim/init.vim &>/dev/null \
-    || { mkdir -p ~/.config/nvim; ln -s `pwd`/vimrc ~/.config/nvim/init.vim; } \
+    || { mkdir -p ~/.config/nvim; ln -sf "$(pwd)/vimrc" ~/.config/nvim/init.vim; } \
     || { echo "Can't link symlink neovim config"; exit 1; }
 
-which brew \
-    || { echo "Please install homebrew first"; exit 1; }
+if [ -z "$(command -v brew)" ]; then
+    "Please install homebrew first"
+    exit 1;
+fi
 
 echo "Brew update & cleanup."
 brew update && brew cleanup \
-    || { echo "Need root to fix homebrew"; sudo chown -R $(whoami) $(brew --prefix)/* ; } \
+    || { echo "Need root to fix homebrew"; sudo chown -R "$(whoami)" "$(brew --prefix)/*" ; } \
     || brew update && brew cleanup \
     || exit 1
 
@@ -30,7 +32,7 @@ BREW_PKGS=( git cmake )
 
 for PKG_NAME in "${BREW_PKGS[@]}"; do
     echo "Checking for ${PKG_NAME}."
-    which "${PKG_NAME}" &>/dev/null || brew list "${PKG_NAME}" &>/dev/null || brew install "${PKG_NAME}" --force-bottle || exit 1
+    command -v "${PKG_NAME}" &>/dev/null || brew list "${PKG_NAME}" &>/dev/null || brew install "${PKG_NAME}" --force-bottle || exit 1
 done
 
 echo "Checking for universal-ctags"
@@ -43,56 +45,44 @@ ls ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf &>/dev/null \
     -o ~/Library/Fonts/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf \
     || exit 1
 
-echo "Checking python2"
-which python2 &>/dev/null || \
-    which python2.7 &>/dev/null || \
-    brew list python2 &>/dev/null || \
-    brew install python2
+echo "Checking python2 (homebrew)"
+brew list python2 &>/dev/null \
+    || brew install python2 \
+    || { echo "Couldn't install homebrew's python2"; exit 1; }
 
-which python2 &>/dev/null || \
-    which python2.7 &>/dev/null && \
-    ln -s `which python2.7` /usr/local/bin/python2
+ln -sf "$(brew --prefix python2)/bin/python2" /usr/local/bin/python2 \
+    || { echo "Couldn't link python2!"; exit 1; }
 
-which python2 &>/dev/null || \
-    { echo "Couldn't install python2!"; exit 1; }
+ln -sf "$(brew --prefix python2)/bin/pip2" /usr/local/bin/pip2 \
+    || { echo "Couldn't link pip2!"; exit 1; }
 
-echo "Checking python3"
-which python3 &>/dev/null || \
-    which python3.7 &>/dev/null || \
-    brew list python3 &>/dev/null || \
-    brew install python3
+echo "Checking python3 (homebrew)"
+brew list python3 &>/dev/null \
+    || brew install python3 \
+    || { echo "Couldn't install homebrew's python3"; exit 1; }
 
-which python3 &>/dev/null || \
-    which python3.7 &>/dev/null && \
-    ln -s `which python3.7` /usr/local/bin/python3
+ln -sf "$(brew --prefix python3)/bin/python3" /usr/local/bin/python3 \
+    || { echo "Couldn't link python3!"; exit 1; }
 
-which python3 &>/dev/null || \
-    { echo "Couldn't install python3!"; exit 1; }
+ln -sf "$(brew --prefix python3)/bin/pip3" /usr/local/bin/pip3 \
+    || { echo "Couldn't link pip3!"; exit 1; }
 
-echo "Checking pip2"
-which pip2 &>/dev/null || \
-    which easy_install-2.7 &>/dev/null && \
-    easy_install-2.7 -U --user pip && \
-    ln -s ~/Library/Python/2.7/bin/pip /usr/local/bin/pip2
-
-which pip2 &>/dev/null || \
-    { echo "Can't find pip2 command, check pip version and link"; exit 1; }
+command -v pip2 &>/dev/null || \
+    { echo "Can't find pip2 command, check pip version and link it"; exit 1; }
     
-echo "Checking pip3"
-which pip3 &>/dev/null || \
-    which easy_install-3.7 &>/dev/null && \
-    easy_install-3.7 -U --user pip && \
-    ln -s ~/Library/Python/3.7/bin/pip /usr/local/bin/pip3
-
-which pip3 &>/dev/null || \
+command -v pip3 &>/dev/null || \
     { echo "Can't find pip3 command, check pip version and link"; exit 1; }
 
-echo "Python helper packages"
-pip2 show pynvim &>/dev/null || pip2 install --user pynvim || exit 1
-pip3 show pynvim &>/dev/null || pip3 install --user pynvim || exit 1
-pip3 show pylint &>/dev/null || pip3 install --user pylint || exit 1
+echo "Updating Python pip"
+pip2 install -U --user pip || exit 1
+pip3 install -U --user pip || exit 1
 
-echo "NeoVim PlugInstall..."
+echo "Python helper packages"
+pip2 install -U --user pynvim || exit 1
+pip3 install -U --user pynvim || exit 1
+pip3 install -U --user pylint || exit 1
+
+echo "NeoVim PlugInstall."
 if [ -f ~/.vim/plugged/cpsm/bin/cpsm_cli ]; then
     nvim --headless +PlugInstall +qa
 else
